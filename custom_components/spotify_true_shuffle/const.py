2cf8f6c83f71e908e57b1d9d5c8e08b6f4930040
rolex86 <92719346+rolex86@users.noreply.github.com>
2026-09-10 18:46:14 +0200

    Add integration constants

---
+++ custom_components/spotify_true_shuffle/const.py
@@ -0,0 +1,26 @@
+DOMAIN = "spotify_true_shuffle"
+PLATFORMS = ["sensor", "button"]
+
+CONF_SPOTIFYPLUS_ENTITY = "spotifyplus_entity"
+CONF_SOURCE_PLAYLIST = "source_playlist"
+CONF_TARGET_PLAYLIST = "target_playlist"
+CONF_ARTIST_GAP = "artist_gap"
+CONF_ALBUM_GAP = "album_gap"
+CONF_MIN_SECONDS = "min_seconds"
+CONF_MIN_PERCENT = "min_percent"
+CONF_SYNC_INTERVAL = "sync_interval_minutes"
+CONF_POLL_INTERVAL = "poll_interval_seconds"
+CONF_CONTEXT_ONLY = "context_only"
+CONF_AUTO_NEW_CYCLE = "auto_new_cycle"
+
+DEFAULT_ARTIST_GAP = 5
+DEFAULT_ALBUM_GAP = 2
+DEFAULT_MIN_SECONDS = 30
+DEFAULT_MIN_PERCENT = 25
+DEFAULT_SYNC_INTERVAL = 15
+DEFAULT_POLL_INTERVAL = 10
+DEFAULT_CONTEXT_ONLY = True
+DEFAULT_AUTO_NEW_CYCLE = True
+
+SPOTIFYPLUS_DOMAIN = "spotifyplus"
+STORE_VERSION = 1
